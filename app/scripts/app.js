542ca1f8a8076473f9b2297867cd1c11119c7ac0
Nicolas Andres Mijoch <ZZJ019@laam.corp.gm.com>
2016-06-30 19:00:29 -0300

    Frist Commit.

---
+++ app/scripts/app.js
@@ -0,0 +1,42 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name angularYoemanLearningApp
+ * @description
+ * # angularYoemanLearningApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('angularYoemanLearningApp', [
+    'ngAnimate',
+    'ngAria',
+    'ngCookies',
+    'ngMessages',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .when('/proveedores', {
+        templateUrl: 'views/proveedores.html',
+        controller: 'ProveedoresCtrl',
+        controllerAs: 'proveedores'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angularYoemanLearningApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the angularYoemanLearningApp
+ */
+angular.module('angularYoemanLearningApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
